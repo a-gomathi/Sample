cea73b4ae47b422537cd8dd0255fee66037701be
a-gomathi <65897231+a-gomathi@users.noreply.github.com>
2021-03-05 14:06:27 +0530

    Update branch.js

--- branch.js
+++ branch.js
@@ -1,20 +1,20 @@
 const user = 'gomathia'
 console.log("process.env.myPassword \t: " + process.env.myPassword);
 
-const without = {
+const options1 = {
       method: 'GET',
       uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
       headers: {
         'Authorization': 'Basic ' + Buffer.from(user + ':' + process.env.myPassword)
       }
     }
-console.log("\n options without base64\t: " + JSON.stringify(without));
+console.log("\n options without base64\t: " + JSON.stringify(options1));
 
-const with = {
+const options2 = {
       method: 'GET',
       uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
       headers: {
         'Authorization': 'Basic ' + Buffer.from(user + ':' + process.env.myPassword).toString('base64')
       }
     }
-console.log("\n options with base64\t: " + JSON.stringify(with));
+console.log("\n options with base64\t: " + JSON.stringify(options2));

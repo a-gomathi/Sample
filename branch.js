56223a18eb2c824c38d214f9499cb64c74951506
a-gomathi <65897231+a-gomathi@users.noreply.github.com>
2021-03-05 14:03:29 +0530

    Update branch.js

--- branch.js
+++ branch.js
@@ -1,3 +1,20 @@
-const envOne = process.env.myPassword;
+const user = 'gomathia'
+console.log("process.env.myPassword \t: " + process.env.myPassword);
 
-console.log("envOne: " + envOne);
+const options = {
+      method: 'GET',
+      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(user + ':' + process.env.myPassword)
+      }
+    }
+console.log("\n options without base64\t: " + JSON.stringify(options));
+
+const options = {
+      method: 'GET',
+      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(user + ':' + process.env.myPassword).toString('base64')
+      }
+    }
+console.log("\n options with base64\t: " + JSON.stringify(options));
